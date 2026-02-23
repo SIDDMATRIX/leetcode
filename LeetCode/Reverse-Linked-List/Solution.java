6cec006ccce7663f7fab5ddb526c4bd15d256a2c
SIDDMATRIX <167300236+SIDDMATRIX@users.noreply.github.com>
2026-02-23 14:58:57 +0530

    Add solution for Reverse Linked List

---
+++ LeetCode/Reverse-Linked-List/Solution.java
@@ -0,0 +1,25 @@
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode reverseList(ListNode head) {
+13        
+14        ListNode prev = null ;
+15        ListNode curr = head ;
+16        while (curr != null){
+17            ListNode NextTemp = curr.next ;
+18            curr.next = prev ;
+19            prev =curr;
+20            curr = 
+21            NextTemp ;
+22        }
+23        return prev ;
+24    }
+25}
