6a97ef7c761d3f8e438678262a4ff2f0598fda88
SIDDMATRIX <167300236+SIDDMATRIX@users.noreply.github.com>
2026-08-03 15:57:22 +0530

    Add solution for String to Integer (atoi)

--- LeetCode/String-to-Integer-(atoi)/Solution.java
+++ LeetCode/String-to-Integer-(atoi)/Solution.java
@@ -1,31 +1,24 @@
 1class Solution {
 2    public int myAtoi(String s) {
-3        int i = 0, n = s.length();
-4        int sign = 1;
-5        int result = 0;
+3        int i = 0, sign = 1, res = 0, n = s.length();
+4
+5        while (i < n && s.charAt(i) == ' ') i++;
 6
-7        while (i < n && s.charAt(i) == ' ') {
-8            i++;
-9        }
-10
-11        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-12            sign = (s.charAt(i) == '-') ? -1 : 1;
-13            i++;
-14        }
-15
-16        while (i < n && Character.isDigit(s.charAt(i))) {
-17            int digit = s.charAt(i) - '0';
-18
-19            if (result > (Integer.MAX_VALUE - digit) / 10) {
-20                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-21            }
-22
-23            result = result * 10 + digit;
-24            i++;
-25        }
-26
-27       
-28        return result * sign;
-29    }
-30}
-31
+7        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-'))
+8            sign = s.charAt(i++) == '-' ? -1 : 1;
+9
+10        while (i < n) {
+11            char c = s.charAt(i);
+12            if (c < '0' || c > '9') break;
+13
+14            int d = c - '0';
+15            if (res > (Integer.MAX_VALUE - d) / 10)
+16                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+17
+18            res = res * 10 + d;
+19            i++;
+20        }
+21
+22        return res * sign;
+23    }
+24}
