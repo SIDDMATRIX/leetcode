0e213fa5aedee8864fe4bf197cad29cee1abdd33
SIDDMATRIX <167300236+SIDDMATRIX@users.noreply.github.com>
2026-08-14 15:20:46 +0530

    Time: 172 ms (22.18%) | Memory: 48.2 MB (7.24%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String longestPalindrome(String s) {
+
+        int n = s.length();
+
+        boolean[][] dp = new boolean[n][n];
+
+        int start = 0;
+        int maxLen = 1;
+
+        for (int gap = 0; gap < n; gap++) {
+
+            for (int i = 0, j = gap; j < n; i++, j++) {
+
+                if (gap == 0) {
+                    dp[i][j] = true;
+                }
+                else if (gap == 1) {
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                }
+                else {
+                    dp[i][j] =
+                        s.charAt(i) == s.charAt(j)
+                        && dp[i + 1][j - 1];
+                }
+
+                if (dp[i][j] && gap + 1 > maxLen) {
+                    start = i;
+                    maxLen = gap + 1;
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+}
