28d9a924ed90ac4f42d8150ee74d6c601515bef8
SIDDMATRIX <167300236+SIDDMATRIX@users.noreply.github.com>
2026-08-24 14:39:19 +0530

    Time: 19 ms (34.64%) | Memory: 50.6 MB (85.21%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,14 +1,34 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int []row : matrix){
-            for ( int x : row){
-                pq.add(x);
+
+        int n = matrix.length;
+
+        PriorityQueue<int[]> pq =
+            new PriorityQueue<>((a, b) ->
+                matrix[a[0]][a[1]] - matrix[b[0]][b[1]]);
+
+        // Add first element of every row
+        for (int i = 0; i < n; i++) {
+            pq.add(new int[]{i, 0});
+        }
+
+        // Remove smallest k-1 elements
+        while (k-- > 1) {
+            int[] curr = pq.remove();
+
+            int row = curr[0];
+            int col = curr[1];
+
+            col++;
+
+            // Add next element from same row
+            if (col < n) {
+                pq.add(new int[]{row, col});
             }
         }
-        for (int i =1;i<k;i ++){
-            pq.poll();
-        }
-        return pq.peek();
+
+        int[] ans = pq.peek();
+
+        return matrix[ans[0]][ans[1]];
     }
 }
