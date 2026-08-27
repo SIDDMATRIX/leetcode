8a5d3bfee28457a604236c9b369deefcb2d0dab8
Siddharth Yadav <167300236+SIDDMATRIX@users.noreply.github.com>
2026-08-27 15:25:44 +0530

    Time: 30 ms (80.28%) | Memory: 64.2 MB (28.51%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,28 +1,25 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        int n = s.length();
-        int[][] dp = new int[n][n];
-
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
-
-        return lps(s, 0, n - 1, dp);
+        int[][] dp = new int[s.length()][s.length()];
+        for(int i = 0; i < dp.length; i++) {
+            for(int j = 0; j < dp.length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return lps(s, 0, s.length() - 1, dp);
     }
-
     public int lps(String s, int si, int ei, int[][] dp) {
-
-        if (si > ei) return 0;
-        if (si == ei) return 1;
-
-        if (dp[si][ei] != -1)
-            return dp[si][ei];
-
-        if (s.charAt(si) == s.charAt(ei))
-            return dp[si][ei] = 2 + lps(s, si + 1, ei - 1, dp);
-
-        return dp[si][ei] = Math.max(
-            lps(s, si + 1, ei, dp),
-            lps(s, si, ei - 1, dp)
-        );
+        if(si >= ei) return (si == ei) ? 1 : 0;
+        if(dp[si][ei] != -1) return dp[si][ei];
+        if(s.charAt(si) == s.charAt(ei)) {
+            int recAns = lps(s, si + 1, ei - 1, dp);
+            dp[si][ei] = recAns + 2;
+            return recAns + 2;
+        }else {
+            int skipS = lps(s, si + 1, ei, dp);
+            int skipE = lps(s, si, ei - 1, dp);
+            dp[si][ei] = Math.max(skipS, skipE); 
+            return Math.max(skipS, skipE);
+        }
     }
 }
